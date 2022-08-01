614306d7ff52aff84e93a53a958a693bdc1e3b05
AnissaBel <99272330+AnissaBel@users.noreply.github.com>
2022-08-01 22:33:10 +0200

    menu mobile toggle

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-
-
-window.onload=function() {
-
-    const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav');
-    const menu_links = document.querySelector('.menu-links');
-    
-
-
-    menu_btn.addEventListener('click',function() {
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
-        menu_links.classList.toggle('is-active');
         
-    });
 
-}
+  //----------NAV BURGER ----------------------//
 
-$(document).on('click','.mobile-nav' ,function(e) {
-    if($(e.target).is('a') ) {
-        $(this).toggle('is-active');
-    }
-});
+  var nav = document.getElementById("topNav");
+  var main = document.getElementById("main");
+  var menu = document.getElementsByClassName("menuitems");
+  var close = document.getElementById("closebtn");
+
+
+  //default to measure if/else from
+  nav.style.height = "60px";
+  main.style.marginTop = "60px";
+  for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
+
+  close.addEventListener("click", function(){
+      var menuIcon = close.children;
+      for (i = 0; i < menuIcon.length; i++){
+          menuIcon[i].classList.toggle("active");
+      }   
+  });
+
+  function navToggle() {	
+      //to close
+      if (nav.style.height <= "280px") {
+          nav.style.height = "60px";
+          main.style.marginTop = "60px";
+      
+          var i = 0;
+          for (i = 0; i < menu.length; i++){
+              menu[i].style.opacity="0.0";
+              menu[i].style.marginTop="100px";
+          };
+          document.body.style.backgroundColor = "rgba(0,0,0,)";
+      
+      } 
+      //to open
+      else if (nav.style.height <= "60px") {
+          nav.style.height = "280px";
+          main.style.marginTop = "0px";
+          var i = 0;
+          for (i = 0; i < menu.length; i++){
+              menu[i].style.opacity="1.0";
+              menu[i].style.marginTop="0px";
+          };
+          document.body.style.backgroundColor = "rgba(0,0,0,)";
+      
+      }
+
+      $(".menuitems").on("click", function(){
+          $("#closebtn span").removeClass("active")
+          
+
+      });
+
+  };
