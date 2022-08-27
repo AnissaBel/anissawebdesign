aa6105075fdc9cf0087af63a99047f7b63911e7d
AnissaBel <99272330+AnissaBel@users.noreply.github.com>
2022-08-27 13:40:23 +0200

    Formulaire PHP

---
+++ action.php
@@ -0,0 +1,17 @@
+<?php
+
+//Si le formulaire a été soumis
+if (isset($_POST["message"])) {
+    $message ="Ce message a été envoyé via la page contact du site 
+    anissawebdesign.com
+    Nom: " . $_POST["nom"] . "
+    Email: " . $_POST["email"] . "
+    Message: " . $_POST["message"];
+
+    $retour = mail("anissa113@hotmail.com", $_POST["sujet"], $message,
+    "From:contact@anissawebdesign.com" . "\r\n" . "Reply-to:" . $_POST["email"]);
+    if ($retour) {
+        echo "<p> Votre message a bien été envoyé ! </p>";
+    }
+}
+?>
